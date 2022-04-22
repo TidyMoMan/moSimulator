1813bed6a50d4be34370bfe04f74905fd2075b32
unknown <mosesdilts@gmail.com>
2022-04-22 12:33:14 -0400

    dfssdffd

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-setInterval(()=>{console.log(yeahhh)},5000)
+var scene = new Phaser.Scene("game");
+var config = {
+    type: Phaser.AUTO,
+    width: 1320,
+    height: 700,
+    type: Phaser.AUTO,
+    parent: 'game',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            //gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+
+    },
+    // scene: { 
+    //     preload: preload,
+    //     create: create,
+    //     update: update
+    // }
+    // OLD WAY OF MANAGING SCENES
+};
+
+
+
+var game = new Phaser.Game(config);
+
+//var thisIsAGoodPlaceToPutPublicVariables;
+
+scene.preload = function() {
+    
+}
+
+scene.create = function() {
+    const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+    const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
+
+}
+
+scene.update = function() {
+    
+}
+game.scene.add("game", scene);
+game.scene.start('game');
